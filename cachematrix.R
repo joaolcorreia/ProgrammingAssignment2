fa908d7629f3fc1098802cc7a68964a4296e3753
Joao Correia <joao.correia@gmail.com>
2014-06-13 02:40:30 -0700

    Programming Assignment 2 - get, and set
    makeCacheMatrix get and set functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse.
+## Inversable Matrix c=rbind(c(4, 3), c(3, 2)) 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        ## gets the value of the matrix
+        get <- function() m
+        
+        ## sets the value 
+        set <- function(y) {
+                m <<- y                
+        } 
+        
+        ## Makes the functions inside the makeCacheMatrix available
+        list(set = set, get = get)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        
 }
