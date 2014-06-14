13498df86da46c5c18e1c64e1c7bea7c6374c063
Joao Correia <joao.correia@gmail.com>
2014-06-13 23:36:39 -0700

    Programming Assignment 2 - Cache working
    Cache working. Assignment complete

--- cachematrix.R
+++ cachematrix.R
@@ -2,16 +2,25 @@
 ## Inversable Matrix c=rbind(c(4, 3), c(3, 2)) 
 
 makeCacheMatrix <- function(m = matrix()) {
+        
         ## gets the value of the matrix
         get <- function() m
         
         ## sets the value 
         set <- function(y) {
-                m <<- y                
+                m <<- y
+                ## Need to set the object null otherwise cacheSolve wont validate is.null
+                mi <<- NULL
         } 
+
+        ## get inverse matrix mi
+        getinverse<-function() mi
+        
+        ## set inverse matrix mi
+        setinverse<-function(x) mi<<-x
         
         ## Makes the functions inside the makeCacheMatrix available
-        list(set = set, get = get)
+        list(set = set, get = get, getinverse = getinverse, setinverse = setinverse)
 }
 
 
@@ -19,7 +28,29 @@ makeCacheMatrix <- function(m = matrix()) {
 ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
 ## should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(m, ...) {
+        ## Get matrix inverse
+        mi <- m$getinverse()
+        
+        ## If the inverse matrix is not null we return it (mi) from global environment
+        if(!is.null(mi)) {
+                message("getting cached matrix")
+                return(mi)
 
+        } else {
+        
+        ## The inverse is null. We need to calculate it
+        ## Get the main matrix
+        x <- m$get()
+        ## Calculate the inverse matrix
+        inv <- solve(x)%*%x
+        ## Set the inverse matrix
+        m$setinverse(inv)
+                
+        }
         
 }
+
+
+
+
